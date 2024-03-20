565201021df426356862287f1fc57a7b535e40ab
Vlad Babii <vlad@babii.ro>
2024-03-20 10:39:09 +0200

    working

--- keyboards/handwired/dactyl_manuform/4x5_5_direct_pico/config.h
+++ keyboards/handwired/dactyl_manuform/4x5_5_direct_pico/config.h
@@ -3,5 +3,9 @@
 
 #pragma once
 
-#define EE_HANDS
-#define SPLIT_USB_DETECT
+// #define EE_HANDS
+// #define SPLIT_USB_DETECT
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25 // Specify a optional status led by GPIO number which blinks when entering the bootloader
--- keyboards/handwired/dactyl_manuform/4x5_5_direct_pico/rules.mk
+++ keyboards/handwired/dactyl_manuform/4x5_5_direct_pico/rules.mk
@@ -1,12 +1,16 @@
 # Build Options
 
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes
+SPLIT_KEYBOARD = no
