caf256f28587a8c3bd3fcdca6a4444519e12f3b7
Vlad Babii <vlad@babii.ro>
2024-03-20 19:38:37 +0200

    working with via

---
+++ keyboards/handwired/dactyl_manuform/4x5_5_direct_pico_easymaker/build.sh
@@ -0,0 +1,4 @@
+qmk compile --keymap via
+qmk compile --keymap default
+rm /mnt/d/kb/cosmotyl-4x5-th-curved-6.h/qmk/right/*.uf2
+cp ~/qmk_firmware/.build/*.uf2 /mnt/d/kb/cosmotyl-4x5-th-curved-6.h/qmk/right
